62e344368795d640a39874b3d37f5de6d56ea853
Agostinho da Silva Milagres <38025023+agostin-miracle@users.noreply.github.com>
2022-07-17 21:14:36 -0300

    Add files via upload

---
+++ Enumerations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeFire.Enums
+{
+    /// <summary>
+    /// Data Providers
+    /// </summary>
+    public enum DataAccessProviderTypes
+    {
+        /// <summary>
+        /// SQL SERVER
+        /// </summary>
+        SqlServer=1,
+        /// <summary>
+        ///  SQL LITE
+        /// </summary>
+        SqLite,
+        /// <summary>
+        /// MYSQL
+        /// </summary>
+        MySql,
+        /// <summary>
+        /// POSTGREE
+        /// </summary>
+        PostgreSql,
+        /// <summary>
+        /// OLEDB
+        /// </summary>
+        OleDb,
+#if NETFULL
+        
+    SqlServerCompact
+#endif
+    }
+
+}
